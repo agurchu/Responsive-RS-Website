e90552eb8519d2ddf98de0895ef67e9d25844ef0
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-06 10:44:00 +0200

    js value accordion implementation

--- script.js
+++ script.js
@@ -20,3 +20,35 @@ var swiperPopular = new Swiper(".popular__container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//_____________________ value accordion ___________________
+
+const accordionItems = document.querySelectorAll(".value__accordion-item");
+
+//create a function called item for each accordionItems that creates a const varieble that will select value__accordion-header
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector(".value__accordion-header");
+
+  //the const varieble will listen to a click
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector(".value__accordion-content");
+
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
+  }
+};
