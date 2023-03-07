549660c8e1f37818ca9f91e7f5bdb6f8dd30dcaf
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-03-07 09:56:58 +0200

    js scroll sections active linkS

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ var swiperPopular = new Swiper(".popular__container", {
 });
 
 //_____________________ value accordion ___________________
+
 ///The code selects all the DOM elements with the class "value__accordion-item" and stores them in the variable "accordionItems".
 const accordionItems = document.querySelectorAll(".value__accordion-item");
 
@@ -52,3 +53,29 @@ const toggleItem = (item) => {
     item.classList.add("accordion-open");
   }
 };
+
+//_____________________ scroll sections active linkS ___________________
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectioId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a [href*= " + sectioId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a [href*= " + sectioId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
